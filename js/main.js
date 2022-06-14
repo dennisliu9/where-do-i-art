82986e4fa10c2d36691c94984429027699dcf320
Dennis Liu <dennis.liu9@gmail.com>
2022-06-14 22:23:58 +0000

    Added event listener to main button to switch views, removed leftover inline styling

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+// Swap view
+function swapView(dataView) {
+  var $views = document.querySelectorAll('[data-view]');
+  for (var vwIdx = 0; vwIdx < $views.length; vwIdx++) {
+    if ($views[vwIdx].dataset.view === dataView) {
+      $views[vwIdx].classList.remove('hidden');
+    } else {
+      $views[vwIdx].classList.add('hidden');
+    }
+  }
+}
+
+// Show Something button switches to selection view
+var $showSomething = document.querySelector('#show-something');
+$showSomething.addEventListener('click', function (event) {
+  swapView(event.target.getAttribute('data-view-link'));
+});
+
 // Metropolitan Museum of Art API
 
 var metEndpoint = 'https://collectionapi.metmuseum.org/public/collection/v1/';
