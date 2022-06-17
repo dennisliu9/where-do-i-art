52a560234c824aeac692ce87e38c328c5f70dabe
Dennis Liu <dennis.liu9@gmail.com>
2022-06-17 10:16:11 +0000

    Added local storage to save user choices, added function to override local storage and start fresh on next reload

--- js/data.js
+++ js/data.js
@@ -1,7 +1,28 @@
 /* exported data */
 
+var localStorageKey = 'ajax-data';
+var devLocalStorageOverrideFlag = false;
 var data = {
   shownObjectIds: [],
   likedObjects: [],
   dislikedObjects: []
 };
+
+if (localStorage.getItem(localStorageKey) !== null) {
+  data = JSON.parse(localStorage.getItem(localStorageKey));
+}
+
+window.addEventListener('beforeunload', function (event) {
+  if (devLocalStorageOverrideFlag) {
+    return;
+  }
+  this.localStorage.setItem(localStorageKey, JSON.stringify(data));
+});
+
+// eslint-disable-next-line no-unused-vars
+function devOverrideLocalStorage() {
+  // Clears out data and prevents data from being persisted in localStorage
+  // To be manually run in the console
+  devLocalStorageOverrideFlag = true;
+  localStorage.removeItem(localStorageKey);
+}
